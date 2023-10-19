a27a8f4e4a0e215ab51d50f6a858d1e49ba85455
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-10-20 02:40:38 +0600

    onclick details try

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
     <h2 className='md:text-5xl text-2xl font-bold text-center py-5 uppercase text-orange-700'>REst api data</h2>
+    
      <LoadData></LoadData>
     </>
   )
--- src/cmponent/LoadData.jsx
+++ src/cmponent/LoadData.jsx
@@ -12,6 +12,7 @@ function LoadData() {
   }, []);
   return (
     <div className=" w-11/12 mx-auto grid lg:grid-cols-3 md:grid-cols-2 gap-5">
+        <Details></Details>
       {country.map((item) => (
         <DisplayData data={item}></DisplayData>
       ))}
@@ -20,9 +21,11 @@ function LoadData() {
 }
 
 function DisplayData(props) {
-  const { capital, name, flags } = props.data;
+  const { name, flags,ccn3 } = props.data;
+  
 
   return (
+    
     <div className=" bg-slate-300 flex flex-col rounded-lg overflow-hidden border-2 shadow-black shadow-lg border-slate-200">
         <div className="bg-black ">
             <img className="w-full sm:h-60 h-48" src={flags.png} alt='img' />
@@ -31,9 +34,38 @@ function DisplayData(props) {
         <div className="p-4 basis-1/2">
             <h2 className="text-2xl font-bold capitalize pb-2 text-orange-700">{name.common}</h2>
             <p className="pb-2 text-base font-semibold">if you want see more details follow the button</p>
-            <button className="btn btn-primary w-full">Details</button>
+            <button onClick={Details(`${ccn3}`)} className="btn btn-primary w-full">Details</button>
         </div>
     </div>
-  );
+  )
+}
+
+function Details(props) {
+    const [data, setdata] = useState([]);
+
+    fetch(`https://restcountries.com/v3.1/alpha/${props}`)
+    .then((res) => res.json())
+    .then(data =>setdata(data[0]))
+    return(
+        <div>
+        <h2>mujahid</h2>
+        {data.map((item) => (
+        <Displaydetails data={item}></Displaydetails>
+      ))}
+        </div>
+    )
+}
+
+
+
+function Displaydetails(props) {
+    const { name, flags,ccn3 } = props.data;
+
+return(
+    <div>
+        <h2>{name.common}</h2>
+    </div>
+)
+    
 }
 export default LoadData;
