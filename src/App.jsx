a83c4d732c2c88aed289d72447df29a5b70cf0cc
mujahidtemp404 <mujahidislam400301@gmail.com>
2023-10-22 03:19:41 +0600

    variable tailwind use

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import './App.css'
 import LoadData from './cmponent/LoadData'
 
 function App() {
+  const h2 ='md:text-5xl text-2xl font-bold text-center py-5 uppercase text-slate-700';
   return (
+
     <>
-    <h2 className='md:text-5xl text-2xl font-bold text-center py-5 uppercase text-orange-700'>REst api data</h2>
+    <h2 className={`${h2}`}>REst api data</h2>
      <LoadData></LoadData>
     </>
   )
